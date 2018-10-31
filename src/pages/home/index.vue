e15083731a1b93e6b04c163e6fb318bc1fb15375
danchaofan <danchaofan@xiangwushuo.com>
2018-10-31 15:12:01 +0800

    新增element组件库

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,16 +1,32 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <el-input-number v-model="num1"
+                     @change="handleChange"
+                     :min="1"
+                     :max="10"
+                     label="描述文字"></el-input-number>
     <h2>hello world</h2>
   </div>
 </template>
 
 <script>
+import { InputNumber } from 'element-ui'
+
 export default {
   name: 'Home',
+  components: {
+    InputNumber
+  },
   data () {
     return {
-      msg: '这个是home/index页面'
+      msg: '这个是home/index页面',
+      num1: 1
+    }
+  },
+  methods: {
+    handleChange (value) {
+      console.log(value)
     }
   }
 }
