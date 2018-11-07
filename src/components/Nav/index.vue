d4859973dc25a146bb88e277b6bb5ca544f0c0ed
danchaofan <danchaofan@xiangwushuo.com>
2018-11-07 20:21:15 +0800

    add:增加less，页面自动化刷新work

--- src/components/Nav/index.vue
+++ src/components/Nav/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="nav">
+    <div class="nav1">
+      nav1
+      <div class="nav2">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,5 +23,18 @@ export default {
 <style scoped>
 .nav {
   background-image: url(https://upload-images.jianshu.io/upload_images/26865-13409e03aa025ca6.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240);
+}
+</style>
+
+<style lang="less" scoped>
+.nav {
+  // background-image: url(https://upload-images.jianshu.io/upload_images/26865-13409e03aa025ca6.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240);
+  background: red;
+  .nav1 {
+    background: black;
+    .nav2 {
+      background: blue;
+    }
+  }
 }
 </style>
